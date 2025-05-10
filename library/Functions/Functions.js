cdbe88c92e1f41659fe67314750c501e944ba957
eroinnovations <eroinnovations9@gmail.com>
2025-05-10 18:12:50 +0300

    Development Connection

--- library/Functions/Functions.js
+++ library/Functions/Functions.js
@@ -2182,15 +2182,35 @@ const PROJECTUPDATE=(callback)=>{
 };
 const AUTOSTART=()=>{
 
-    import('https://eroinnovations.github.io/Elite-Robust-Ontology/Start/Start.js')
-    .then(module => {
-          if (typeof module.START === 'function') {
-            module.START();
-          } else {
-            console.error('START is not defined in the module');
-          }
-    })
-    .catch(error => {
-          console.error('Error loading the module:', error);
-    });
+    if (localStorage.getItem('Environment') === 'Development' ) {
+
+        import('../Start/Start.js')
+        .then(module => {
+            if (typeof module.START === 'function') {
+                module.START();
+            } else {
+                console.error('START is not defined in the module');
+            }
+        })
+        .catch(error => {
+            console.error('Error loading the module:', error);
+        });
+        
+    } else {
+
+        import('https://eroinnovations.github.io/Elite-Robust-Ontology/Start/Start.js')
+        .then(module => {
+            if (typeof module.START === 'function') {
+                module.START();
+            } else {
+                console.error('START is not defined in the module');
+            }
+        })
+        .catch(error => {
+            console.error('Error loading the module:', error);
+        });
+        
+    };
+
+
 };
