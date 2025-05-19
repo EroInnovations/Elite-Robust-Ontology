8d48cf54179843bc1d2cccd37d1256cf73a69506
eroinnovations <eroinnovations9@gmail.com>
2025-05-19 20:05:49 +0300

    Updates

---
+++ Build/Qel/Web/Web.js
@@ -0,0 +1,33 @@
+const NOVASTART=()=>{
+
+    ROUTE('',HOMEPAGE,'HOMEPAGE');
+    
+};
+
+const HOMEPAGE=()=>{
+
+    DISPLAY('',`
+
+        <header>
+
+            <h1 class='LeftText' >Qel</h1>
+
+            <p>Products</p>
+
+            <p>About</p>
+
+            <p>Contact</p>
+        
+        </header>
+
+        <div class='HeaderDiv'>
+
+            <h1 class='AppName'>Qel Medistore</h1>
+
+            <p class='Intro'>Qel Medistore is an Online Store that Ventures in Delivery of Medical,Laboratory,Othopedic Equipments with Multiple OutLets Country Wide.</p>
+        
+        </div>
+    
+    `);
+
+};
